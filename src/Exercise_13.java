8956524fa1d5bf0f858fdbecad50383da5b2a86d
Helena Peters <94479329+helenapeters@users.noreply.github.com>
2023-03-08 20:56:18 +0100

    Add files via upload

---
+++ src/Exercise_13.java
@@ -0,0 +1,9 @@
+public class Exercise_13 {
+    public static void main(String[] args) {
+        int K, max_tosses, total_succes;
+        K = 10000;
+        max_tosses = 6;
+        total_succes = 0;
+
+    }
+}
