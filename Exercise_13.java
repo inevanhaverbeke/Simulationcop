ea7f4dd2947a52343438f82d819b973c33c1ee5d
Helena Peters <94479329+helenapeters@users.noreply.github.com>
2023-03-08 20:56:31 +0100

    Delete Exercise_13.java

--- Exercise_13.java
+++
@@ -1,9 +0,0 @@
-public class Exercise_13 {
-    public static void main(String[] args) {
-        int K, max_tosses, total_succes;
-        K = 10000;
-        max_tosses = 6;
-        total_succes = 0;
-
-    }
-}
